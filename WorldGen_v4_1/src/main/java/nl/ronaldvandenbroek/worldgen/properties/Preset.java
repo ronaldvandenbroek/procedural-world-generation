78fc39fc196f0e4e8474e8131546f192a5d385ac
Ronald van den Broek <ronald.vandenbroek@live.nl>
2019-12-07 20:03:51 +0100

    Moved all static variables to property files

---
+++ WorldGen_v4_1/src/main/java/nl/ronaldvandenbroek/worldgen/properties/Preset.java
@@ -0,0 +1,25 @@
+package nl.ronaldvandenbroek.worldgen.properties;
+
+public class Preset {
+    public static String HEIGHT_MAP_BASE_NAME;
+    public static int HEIGHT_MAP_BASE_SEED;
+    public static int HEIGHT_MAP_BASE_OCTAVE;
+    public static float HEIGHT_MAP_BASE_NOISE_FALLOFF;
+    public static float HEIGHT_MAP_BASE_INTENSITY;
+    public static boolean HEIGHT_MAP_BASE_RIDGE;
+    public static float HEIGHT_MAP_BASE_POWER;
+    public static float HEIGHT_MAP_BASE_CIRCULAR_FALLOFF;
+    public static float HEIGHT_MAP_BASE_WEIGHT;
+
+    public static String HEIGHT_MAP_RIDGE_NAME;
+    public static int HEIGHT_MAP_RIDGE_SEED;
+    public static int HEIGHT_MAP_RIDGE_OCTAVE;
+    public static float HEIGHT_MAP_RIDGE_NOISE_FALLOFF;
+    public static float HEIGHT_MAP_RIDGE_INTENSITY;
+    public static boolean HEIGHT_MAP_RIDGE_RIDGE;
+    public static float HEIGHT_MAP_RIDGE_POWER;
+    public static float HEIGHT_MAP_RIDGE_CIRCULAR_FALLOFF;
+    public static float HEIGHT_MAP_RIDGE_WEIGHT;
+
+    public static float HEIGHT_MAP_TOTAL_CIRCULAR_FALLOFF;
+}
