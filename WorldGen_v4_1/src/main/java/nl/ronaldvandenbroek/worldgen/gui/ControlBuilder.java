098110037a0004ee925f0dd8ee947b850877085b
Ronald van den Broek <ronald.vandenbroek@live.nl>
2019-12-07 17:40:15 +0100

    Full switch to Floats in ControlP5

--- WorldGen_v4_1/src/main/java/nl/ronaldvandenbroek/worldgen/gui/ControlBuilder.java
+++ WorldGen_v4_1/src/main/java/nl/ronaldvandenbroek/worldgen/gui/ControlBuilder.java
@@ -5,12 +5,12 @@ import nl.ronaldvandenbroek.worldgen.calculation.HeightMap;
 public class ControlBuilder {
     public static void HeightMap(ControlGui controlGui, HeightMap heightMap) {
         controlGui.createGUISliderTitle(heightMap.getName() + "HeightMap", false);
-        controlGui.createGUISliderFloat(new ControlElementSliderFloat(heightMap.getName() + " Seed", "seed", heightMap, heightMap.getSeed(), 0f, 100f));
-        controlGui.createGUISliderFloat(new ControlElementSliderFloat(heightMap.getName() + " Noise Octaves", "octaves", heightMap, heightMap.getOctaves(), 1f, 15f));
-        controlGui.createGUISliderFloat(new ControlElementSliderFloat(heightMap.getName() + " Noise Intensity", "intensity", heightMap, heightMap.getIntensity(), 1f, 30f));
-        controlGui.createGUISliderFloat(new ControlElementSliderFloat(heightMap.getName() + " Noise Falloff", "noiseFalloff", heightMap, heightMap.getNoiseFalloff(), 0f, 10f));
-        controlGui.createGUISliderFloat(new ControlElementSliderFloat(heightMap.getName() + " Circular Falloff", "circularFalloff", heightMap, heightMap.getCircularFalloff(), 0f, 1f));
-        controlGui.createGUISliderFloat(new ControlElementSliderFloat(heightMap.getName() + " Power", "power", heightMap, heightMap.getPower(), 0f, 50f));
-        controlGui.createGUISliderFloat(new ControlElementSliderFloat(heightMap.getName() + " Weight", "weight", heightMap, heightMap.getWeight(), 0f, 1f));
+        controlGui.createGUISlider(new ControlElementSlider(heightMap.getName() + " Seed", "seed", heightMap, heightMap.getSeed(), 0f, 100f));
+        controlGui.createGUISlider(new ControlElementSlider(heightMap.getName() + " Noise Octaves", "octaves", heightMap, heightMap.getOctaves(), 1f, 15f));
+        controlGui.createGUISlider(new ControlElementSlider(heightMap.getName() + " Noise Intensity", "intensity", heightMap, heightMap.getIntensity(), 1f, 30f));
+        controlGui.createGUISlider(new ControlElementSlider(heightMap.getName() + " Noise Falloff", "noiseFalloff", heightMap, heightMap.getNoiseFalloff(), 0f, 10f));
+        controlGui.createGUISlider(new ControlElementSlider(heightMap.getName() + " Circular Falloff", "circularFalloff", heightMap, heightMap.getCircularFalloff(), 0f, 1f));
+        controlGui.createGUISlider(new ControlElementSlider(heightMap.getName() + " Power", "power", heightMap, heightMap.getPower(), 0f, 50f));
+        controlGui.createGUISlider(new ControlElementSlider(heightMap.getName() + " Weight", "weight", heightMap, heightMap.getWeight(), 0f, 1f));
     }
 }
